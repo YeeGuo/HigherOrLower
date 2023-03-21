d82b6a67d9759b3660cbb483f78f66595c0d6fb7
YeeGuo <kiwi881217@gmail.com>
2023-03-21 16:29:08 +0800

    Initial Commit

---
+++ HigherOrLower/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HigherOrLower
+//
+//  Created by 張凱棋 on 2023/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
